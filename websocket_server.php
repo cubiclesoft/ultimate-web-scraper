61d871bf52512c585c83f48b818d0b3b2d6a53bd
cubiclesoft <webmaster@cubiclesoft.com>
2016-04-18 07:13:33 -0700

    Attempts to load WebSocket class if not loaded.  Switched clients to objects instead of arrays and removed pass-by-reference leak issue(s).  Added translation function WSTranslate.  Added WebServer to WebSocketServer client conversion support.  Made it easy to create multi-server socket select.  Eliminated dependency on HTTP class.

--- websocket_server.php
+++ websocket_server.php
@@ -1,6 +1,6 @@
 <?php
 	// CubicleSoft PHP WebSocketServer class.
-	// (C) 2015 CubicleSoft.  All Rights Reserved.
+	// (C) 2016 CubicleSoft.  All Rights Reserved.
 
 	// Make sure PHP doesn't introduce weird limitations.
 	ini_set("memory_limit", "-1");
@@ -19,6 +19,8 @@
 
 		public function Reset()
 		{
+			if (!class_exists("WebSocket"))  require_once str_replace("\\", "/", dirname(__FILE__)) . "/websocket.php";
+
 			$this->fp = false;
 			$this->clients = array();
 			$this->nextclientid = 1;
@@ -67,7 +69,7 @@
 			$this->Stop();
 
 			$this->fp = stream_socket_server("tcp://" . $host . ":" . $port, $errornum, $errorstr);
-			if ($this->fp === false)  return array("success" => false, "error" => HTTP::HTTPTranslate("Bind() failed.  Reason:  %s (%d)", $errorstr, $errornum), "errorcode" => "bind_failed");
+			if ($this->fp === false)  return array("success" => false, "error" => self::WSTranslate("Bind() failed.  Reason:  %s (%d)", $errorstr, $errornum), "errorcode" => "bind_failed");
 
 			// Enable non-blocking mode.
 			stream_set_blocking($this->fp, 0);
@@ -77,17 +79,18 @@
 
 		public function Stop()
 		{
+			foreach ($this->clients as $client)
+			{
+				if ($client->websocket !== false)  $client->websocket->Disconnect();
+				else  fclose($client->fp);
+			}
+
+			$this->clients = array();
+
 			if ($this->fp !== false)
 			{
-				foreach ($this->clients as $client)
-				{
-					if ($client["websocket"] !== false)  $client["websocket"]->Disconnect();
-					else  fclose($client["fp"]);
-				}
-
 				fclose($this->fp);
 
-				$this->clients = array();
 				$this->fp = false;
 			}
 
@@ -106,15 +109,15 @@
 			$result = "";
 
 			if ($method !== "GET")  $result .= "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
-			else if (!isset($client["headers"]["Host"]) || !isset($client["headers"]["Upgrade"]) || stripos($client["headers"]["Upgrade"], "websocket") === false || !isset($client["headers"]["Connection"]) || stripos($client["headers"]["Connection"], "upgrade") === false || !isset($client["headers"]["Sec-Websocket-Key"]))
+			else if (!isset($client->headers["Host"]) || !isset($client->headers["Connection"]) || stripos($client->headers["Connection"], "upgrade") === false || !isset($client->headers["Upgrade"]) || stripos($client->headers["Upgrade"], "websocket") === false || !isset($client->headers["Sec-Websocket-Key"]))
 			{
 				$result .= "HTTP/1.1 400 Bad Request\r\n\r\n";
 			}
-			else if (!isset($client["headers"]["Sec-Websocket-Version"]) || $client["headers"]["Sec-Websocket-Version"] != 13)
+			else if (!isset($client->headers["Sec-Websocket-Version"]) || $client->headers["Sec-Websocket-Version"] != 13)
 			{
 				$result .= "HTTP/1.1 426 Upgrade Required\r\nSec-WebSocket-Version: 13\r\n\r\n";
 			}
-			else if (!isset($client["headers"]["Origin"]))
+			else if (!isset($client->headers["Origin"]))
 			{
 				$result .= "HTTP/1.1 403 Forbidden\r\n\r\n";
 			}
@@ -128,32 +131,89 @@
 			return "";
 		}
 
+		protected function InitNewClient($fp)
+		{
+			$client = new stdClass();
+
+			$client->id = $this->nextclientid;
+			$client->readdata = "";
+			$client->writedata = "";
+			$client->request = false;
+			$client->path = "";
+			$client->url = "";
+			$client->headers = array();
+			$client->lastheader = "";
+			$client->websocket = false;
+			$client->fp = $fp;
+
+			$this->clients[$this->nextclientid] = $client;
+
+			$this->nextclientid++;
+
+			return $client;
+		}
+
+		private function ProcessInitialResponse($method, $path, $client)
+		{
+			// Let a derived class handle the new connection (e.g. processing Origin and Host).
+			// Since the 'websocketclass' is instantiated AFTER this function, it is possible to switch classes on the fly.
+			$client->writedata .= $this->ProcessNewConnection($method, $path, $client);
+
+			// If an error occurs, the connection will still terminate.
+			$client->websocket = new $this->websocketclass();
+			$client->websocket->SetCloseMode($this->defaultclosemode);
+			$client->websocket->SetKeepAliveTimeout($this->defaultkeepalive);
+
+			// If nothing was output, accept the connection.
+			if ($client->writedata === "")
+			{
+				$client->writedata .= "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n";
+				$client->writedata .= "Sec-WebSocket-Accept: " . base64_encode(sha1($client->headers["Sec-Websocket-Key"] . WebSocket::KEY_GUID, true)) . "\r\n";
+				$client->writedata .= $this->ProcessAcceptedConnection($method, $path, $client);
+				$client->writedata .= "\r\n";
+
+				// Finish class initialization.
+				$client->websocket->SetServerMode();
+				$client->websocket->SetMaxReadFrameSize($this->defaultmaxreadframesize);
+				$client->websocket->SetMaxReadMessageSize($this->defaultmaxreadmessagesize);
+
+				// Set the socket in the WebSocket class.
+				$client->websocket->Connect("", "", "", array("fp" => $client->fp));
+			}
+		}
+
+		public function UpdateStreamsAndTimeout($prefix, &$timeout, &$readfps, &$writefps)
+		{
+			if ($this->fp !== false)  $readfps[$prefix . "ws_s"] = $this->fp;
+			if ($timeout === false || $timeout > $this->defaultkeepalive)  $timeout = $this->defaultkeepalive;
+
+			foreach ($this->clients as $id => $client)
+			{
+				if ($client->writedata === "")  $readfps[$prefix . "ws_c_" . $id] = $client->fp;
+
+				if ($client->writedata !== "" || ($client->websocket !== false && $client->websocket->NeedsWrite()))  $writefps[$prefix . "ws_c_" . $id] = $client->fp;
+
+				if ($client->websocket !== false)
+				{
+					$timeout2 = $client->websocket->GetKeepAliveTimeout();
+					if ($timeout > $timeout2)  $timeout = $timeout2;
+				}
+			}
+		}
+
 		// Handles new connections, the initial conversation, basic packet management, and timeouts.
 		// Can wait on more streams than just sockets and/or more sockets.  Useful for waiting on other resources.
 		// 'ws_s' and the 'ws_c_' prefix are reserved.
 		// Returns an array of clients that may need more processing.
 		public function Wait($timeout = false, $readfps = array(), $writefps = array(), $exceptfps = NULL)
 		{
-			$readfps["ws_s"] = $this->fp;
-			if ($timeout === false || $timeout > $this->defaultkeepalive)  $timeout = $this->defaultkeepalive;
-
-			foreach ($this->clients as $id => &$client)
-			{
-				if ($client["writedata"] === "")  $readfps["ws_c_" . $id] = $client["fp"];
-
-				if ($client["writedata"] !== "" || ($client["websocket"] !== false && $client["websocket"]->NeedsWrite()))  $writefps["ws_c_" . $id] = $client["fp"];
-
-				if ($client["websocket"] !== false)
-				{
-					$timeout2 = $client["websocket"]->GetKeepAliveTimeout();
-					if ($timeout > $timeout2)  $timeout = $timeout2;
-				}
-			}
+			$this->UpdateStreamsAndTimeout("", $timeout, $readfps, $writefps);
 
 			$result = array("success" => true, "clients" => array(), "removed" => array(), "readfps" => array(), "writefps" => array(), "exceptfps" => array());
+			if (!count($readfps) && !count($writefps))  return $result;
 
 			$result2 = @stream_select($readfps, $writefps, $exceptfps, $timeout);
-			if ($result2 === false)  return array("success" => false, "error" => HTTP::HTTPTranslate("Wait() failed due to stream_select() failure.  Most likely cause:  Connection failure."), "errorcode" => "stream_select_failed");
+			if ($result2 === false)  return array("success" => false, "error" => self::WSTranslate("Wait() failed due to stream_select() failure.  Most likely cause:  Connection failure."), "errorcode" => "stream_select_failed");
 
 			// Handle new connections.
 			if (isset($readfps["ws_s"]))
@@ -163,23 +223,10 @@
 					// Enable non-blocking mode.
 					stream_set_blocking($fp, 0);
 
-					$this->clients[$this->nextclientid] = array(
-						"id" => $this->nextclientid,
-						"readdata" => "",
-						"writedata" => "",
-						"request" => false,
-						"path" => "",
-						"url" => "",
-						"headers" => array(),
-						"lastheader" => "",
-						"websocket" => false,
-						"fp" => $fp
-					);
-
-					$this->nextclientid++;
+					$this->InitNewClient($fp);
 				}
 
-				unset($readfps["s"]);
+				unset($readfps["ws_s"]);
 			}
 
 			// Handle clients in the read queue.
@@ -191,7 +238,9 @@
 
 				if (!isset($this->clients[$id]))  continue;
 
-				if ($this->clients[$id]["websocket"] !== false)
+				$client = $this->clients[$id];
+
+				if ($client->websocket !== false)
 				{
 					$this->ProcessClientQueuesAndTimeoutState($result, $id, true, isset($writefps[$cid]));
 
@@ -209,9 +258,9 @@
 					}
 					else
 					{
-						$this->clients[$id]["readdata"] .= $result2;
+						$client->readdata .= $result2;
 
-						if (strlen($this->clients[$id]["readdata"]) > 100000)
+						if (strlen($client->readdata) > 100000)
 						{
 							// Bad header size.  Just kill the connection.
 							@fclose($fp);
@@ -220,29 +269,29 @@
 						}
 						else
 						{
-							while (($pos = strpos($this->clients[$id]["readdata"], "\n")) !== false)
+							while (($pos = strpos($client->readdata, "\n")) !== false)
 							{
 								// Retrieve the next line of input.
-								$line = rtrim(substr($this->clients[$id]["readdata"], 0, $pos));
-								$this->clients[$id]["readdata"] = (string)substr($this->clients[$id]["readdata"], $pos + 1);
+								$line = rtrim(substr($client->readdata, 0, $pos));
+								$client->readdata = (string)substr($client->readdata, $pos + 1);
 
-								if ($this->clients[$id]["request"] === false)  $this->clients[$id]["request"] = trim($line);
+								if ($client->request === false)  $client->request = trim($line);
 								else if ($line !== "")
 								{
 									// Process the header.
-									if ($this->clients[$id]["lastheader"] != "" && (substr($line, 0, 1) == " " || substr($line, 0, 1) == "\t"))  $this->clients[$id]["headers"][$this->clients[$id]["lastheader"]] .= $header;
+									if ($client->lastheader != "" && (substr($line, 0, 1) == " " || substr($line, 0, 1) == "\t"))  $client->headers[$client->lastheader] .= $header;
 									else
 									{
 										$pos = strpos($line, ":");
 										if ($pos === false)  $pos = strlen($line);
-										$this->clients[$id]["lastheader"] = HTTP::HeaderNameCleanup(substr($line, 0, $pos));
-										$this->clients[$id]["headers"][$this->clients[$id]["lastheader"]] = ltrim(substr($line, $pos + 1));
+										$client->lastheader = self::HeaderNameCleanup(substr($line, 0, $pos));
+										$client->headers[$client->lastheader] = ltrim(substr($line, $pos + 1));
 									}
 								}
 								else
 								{
 									// Headers have all been received.  Process the client request.
-									$request = $this->clients[$id]["request"];
+									$request = $client->request;
 									$pos = strpos($request, " ");
 									if ($pos === false)  $pos = strlen($request);
 									$method = (string)substr($request, 0, $pos);
@@ -253,34 +302,12 @@
 									$path = (string)substr($request, 0, $pos);
 									if ($path === "")  $path = "/";
 
-									$this->clients[$id]["path"] = $path;
-									$this->clients[$id]["url"] = "ws://" . $client["headers"]["Host"] . $path;
+									if (isset($client->headers["Host"]))  $client->headers["Host"] = preg_replace('/[^a-z0-9.:\[\]_-]/', "", strtolower($client->headers["Host"]));
 
-									// Let a derived class handle the new connection (e.g. processing Origin and Host).
-									// Since the 'websocketclass' is instantiated AFTER this function, it is possible to switch classes on the fly.
-									$this->clients[$id]["writedata"] .= $this->ProcessNewConnection($method, $path, $this->clients[$id]);
+									$client->path = $path;
+									$client->url = "ws://" . (isset($client->headers["Host"]) ? $client->headers["Host"] : "localhost") . $path;
 
-									// If an error occurs, the connection will still terminate.
-									$this->clients[$id]["websocket"] = new $this->websocketclass();
-									$this->clients[$id]["websocket"]->SetCloseMode($this->defaultclosemode);
-									$this->clients[$id]["websocket"]->SetKeepAliveTimeout($this->defaultkeepalive);
-
-									// If nothing was output, accept the connection.
-									if ($this->clients[$id]["writedata"] === "")
-									{
-										$this->clients[$id]["writedata"] .= "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n";
-										$this->clients[$id]["writedata"] .= "Sec-WebSocket-Accept: " . base64_encode(sha1($this->clients[$id]["headers"]["Sec-Websocket-Key"] . WebSocket::KEY_GUID, true)) . "\r\n";
-										$this->clients[$id]["writedata"] .= $this->ProcessAcceptedConnection($method, $path, $this->clients[$id]);
-										$this->clients[$id]["writedata"] .= "\r\n";
-
-										// Finish class initialization.
-										$this->clients[$id]["websocket"]->SetServerMode();
-										$this->clients[$id]["websocket"]->SetMaxReadFrameSize($this->defaultmaxreadframesize);
-										$this->clients[$id]["websocket"]->SetMaxReadMessageSize($this->defaultmaxreadmessagesize);
-
-										// Set the socket in the WebSocket class.
-										$this->clients[$id]["websocket"]->Connect("", "", "", array("fp" => $fp));
-									}
+									$this->ProcessInitialResponse($method, $path, $client);
 
 									break;
 								}
@@ -301,10 +328,12 @@
 
 				if (!isset($this->clients[$id]))  continue;
 
-				if ($this->clients[$id]["writedata"] === "")  $this->ProcessClientQueuesAndTimeoutState($result, $id, false, true);
+				$client = $this->clients[$id];
+
+				if ($client->writedata === "")  $this->ProcessClientQueuesAndTimeoutState($result, $id, false, true);
 				else
 				{
-					$result2 = @fwrite($fp, $this->clients[$id]["writedata"]);
+					$result2 = @fwrite($fp, $client->writedata);
 					if ($result2 === false || feof($fp))
 					{
 						@fclose($fp);
@@ -313,10 +342,10 @@
 					}
 					else
 					{
-						$this->clients[$id]["writedata"] = (string)substr($this->clients[$id]["writedata"], $result2);
+						$client->writedata = (string)substr($client->writedata, $result2);
 
 						// Let the application know about the new client.
-						if ($this->clients[$id]["writedata"] === "")  $result["clients"][$id] = $this->clients[$id];
+						if ($client->writedata === "")  $result["clients"][$id] = $client;
 					}
 				}
 
@@ -324,9 +353,9 @@
 			}
 
 			// Handle client timeouts.
-			foreach ($this->clients as $id => &$client)
+			foreach ($this->clients as $id => $client)
 			{
-				if (!isset($result["clients"][$id]) && $client["writedata"] === "" && $client["websocket"] !== false)
+				if (!isset($result["clients"][$id]) && $client->writedata === "" && $client->websocket !== false)
 				{
 					$this->ProcessClientQueuesAndTimeoutState($result, $id, false, false);
 				}
@@ -342,23 +371,15 @@
 
 		protected function ProcessClientQueuesAndTimeoutState(&$result, $id, $read, $write)
 		{
-			$result2 = $this->clients[$id]["websocket"]->ProcessQueuesAndTimeoutState($read, $write);
-			if ($result2["success"])  $result["clients"][$id] = $this->clients[$id];
+			$client = $this->clients[$id];
+
+			$result2 = $client->websocket->ProcessQueuesAndTimeoutState($read, $write);
+			if ($result2["success"])  $result["clients"][$id] = $client;
 			else
 			{
-				// Remove the client.
-				if ($this->clients[$id]["websocket"]->GetStream() !== false)
-				{
-					$this->clients[$id]["websocket"]->Disconnect();
-					$this->clients[$id]["websocket"] = false;
-					$this->clients[$id]["fp"] = false;
-				}
+				$result["removed"][$id] = array("result" => $result2, "client" => $client);
 
-				if ($this->clients[$id]["fp"] !== false)  @fclose($this->clients[$id]["fp"]);
-
-				$result["removed"][$id] = array("result" => $result2, "client" => $this->clients[$id]);
-
-				unset($this->clients[$id]);
+				$this->RemoveClient($id);
 			}
 		}
 
@@ -376,18 +397,57 @@
 		{
 			if (isset($this->clients[$id]))
 			{
+				$client = $this->clients[$id];
+
 				// Remove the client.
-				if ($this->clients[$id]["websocket"]->GetStream() !== false)
+				if ($client->websocket->GetStream() !== false)
 				{
-					$this->clients[$id]["websocket"]->Disconnect();
-					$this->clients[$id]["websocket"] = false;
-					$this->clients[$id]["fp"] = false;
+					$client->websocket->Disconnect();
+					$client->websocket = false;
+					$client->fp = false;
 				}
 
-				if ($this->clients[$id]["fp"] !== false)  @fclose($this->clients[$id]["fp"]);
+				if ($client->fp !== false)  @fclose($client->fp);
 
 				unset($this->clients[$id]);
 			}
+		}
+
+		public function ProcessWebServerClientUpgrade($webserver, $client)
+		{
+			if (!($client instanceof WebServer_Client))  return false;
+
+			if (!$client->requestcomplete || $client->mode === "handle_response")  return false;
+			if ($client->request["method"] !== "GET" || !isset($client->headers["Connection"]) || stripos($client->headers["Connection"], "upgrade") === false || !isset($client->headers["Upgrade"]) || stripos($client->headers["Upgrade"], "websocket") === false)  return false;
+
+			// Create an equivalent WebSocket server client class.
+			$webserver->DetachClient($client->id);
+
+			$method = $client->request["method"];
+			$path = $client->request["path"];
+
+			$client2 = $this->InitNewClient($client->fp);
+			$client2->request = $client->request["line"];
+			$client2->headers = $client->headers;
+			$client2->path = $path;
+			$client2->url = "ws://" . (isset($client->headers["Host"]) ? $client->headers["Host"] : "localhost") . $path;
+
+			$this->ProcessInitialResponse($method, $path, $client2);
+
+			return $client2->id;
+		}
+
+		public static function HeaderNameCleanup($name)
+		{
+			return preg_replace('/\s+/', "-", ucwords(strtolower(trim(preg_replace('/[^A-Za-z0-9 ]/', " ", $name)))));
+		}
+
+		public static function WSTranslate()
+		{
+			$args = func_get_args();
+			if (!count($args))  return "";
+
+			return call_user_func_array((defined("CS_TRANSLATE_FUNC") && function_exists(CS_TRANSLATE_FUNC) ? CS_TRANSLATE_FUNC : "sprintf"), $args);
 		}
 	}
 ?>
