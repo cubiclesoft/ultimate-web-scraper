8d1e10aedbbc3f3ccefd4b2eca30ecdf1b7118ab
cubiclesoft <webmaster@cubiclesoft.com>
2018-08-23 08:26:10 -0700

    Removed unnecessary state transformation.

--- web_server.php
+++ web_server.php
@@ -1127,8 +1127,6 @@
 			if ($this->requestcomplete && !$this->responsefinalized)
 			{
 				$this->responsebodysize = $bodysize;
-
-				if ($this->mode !== "handle_response")  $this->mode = "response_ready";
 			}
 		}
 
